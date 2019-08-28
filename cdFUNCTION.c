5682b16af86a8cddc2797df821e4f1af84ea087a
747-diego <725@holbertonschool.com>
2019-08-28 20:27:05 +0000

    wiped out lines of code and comments for betty checks

---
+++ cdFUNCTION.c
@@ -0,0 +1,8 @@
+#include "holberton.h"
+
+int cd(char *path)
+{
+	chdir(path);
+	return (0);
+}
+
--- prompt.c
+++ prompt.c
@@ -16,77 +16,66 @@ void prompt_handler(int sig)
  * @argc: argument count
  * @argv: argument vector
  * @env: environment variables
- *
  * Return: 0 on exit, 1 if any failures happen
  */
 int main(int argc, char **argv, char **env)
 {
-	char *buffer; char **commands;
-	size_t length; ssize_t characters;
-	char *prompt = "$ ", *exit_command = "exit", *env_command = "env";
-	pid_t child; struct stat fileStat;
+	char *buffer,  **commands;
+	size_t length;
+	ssize_t characters;
+	char *prompt = "$ ", *exit_command = "exit",
+	 *env_command = "env", *cd_command = "cd";
+	pid_t child;
+	struct stat fileStat;
 	int status, count;
 	(void)argc;
 
 	buffer = NULL, length = 0, count = 0;
-	/* write prompt only if it's from standard input */
-	if (isatty(STDIN_FILENO))
+	if (isatty(STDIN_FILENO)) /* write prompt only if it's from standard input */
 		write(STDOUT_FILENO, prompt, 2);
-	/*signal kill for ctrl + c */
-	signal(SIGINT, prompt_handler);
-	/* while loop contining forever */
-	while ((characters = getline(&buffer, &length, stdin)))
+	signal(SIGINT, prompt_handler); /*signal kill for ctrl + c */
+	while ((characters = getline(&buffer, &length, stdin))) /* while loop contining forever */
 	{
-		/* checks for end of file */
-		if (characters == EOF)
+		if (characters == EOF) /* checks for end of file */
 			end_of_file(buffer);
-		/*
-		 * counting the number of times the prompt
-		 * shows up to display correct error
-		 */
-		++count;
-		/* collects commands from prompt and store in a double pointer array */
-		commands = array_from_strtok(buffer);
-		/*create a new process */
-		child = fork();
+		++count; /* counting the number of times the prompt shows up to display correct error */
+		commands = array_from_strtok(buffer); /* collects commands from prompt and store in a double pointer array */
+
+		if (_strcmp(cd_command, commands[0]))
+		{
+			if (cd(commands[1]) < 0)
+				perror(commands[1]);
+			continue;
+		}
+		child = fork(); /*create a new process */
 		if (child == -1)
 			fork_fail();
 		if (child == 0)
 		{
-			/* check if commands is NULL or all empty spaces */
-			if (commands == NULL)
+			if (commands == NULL)  /* check if commands is NULL or all empty spaces */
 				command_is_null(buffer);
-			/* check if the command is an EXIT to exit the shell */
-			else if (_strcmp(exit_command, commands[0]))
+			else if (_strcmp(exit_command, commands[0])) /* check if the command is an EXIT to exit the shell */
 				exit_out(buffer, commands);
-			/* check if the command is ENV to print out environment variables */
-			else if (_strcmp(env_command, commands[0]))
+			else if (_strcmp(env_command, commands[0])) /* check if the command is ENV to print out environment variables */
 				env_out(buffer, commands, env);
-			/* check if the command is a full path to an executable file */
-			else if (stat(commands[0], &fileStat) == 0)
+			else if (stat(commands[0], &fileStat) == 0) /* check if the command is a full path to an executable file */
 				execve(commands[0], commands, NULL);
-			/* check all $PATH directories for executable commands */
-		        else
+			else /* check all $PATH directories for executable commands */
 				create_path(commands, buffer, env, argv, count);
 		}
-		/* Free mallocs from tokens we created */
-		else
+		else  /* Free mallocs from tokens we created */
 		{
-			/* wait for the child process to finish */
-			wait(&status);
+			wait(&status);  /* wait for the child process to finish */
 			if (commands == NULL)
 				parent_free_buff_commands(buffer, commands);
-			/* if exit, free buffer and commands, then exit program */
-			else if (_strcmp(exit_command, commands[0]))
+			else if (_strcmp(exit_command, commands[0])) /* if exit, free buffer and commands, then exit program */
 				exit_out(buffer, commands);
-			/*free buffer and commands double ptr from parent process */
-			else
+			else /*free buffer and commands double ptr from parent process */
 				parent_free_buff_commands(buffer, commands);
 		}
-		/* reset length and buffer for getline funciton */
-		length = 0; buffer = NULL;
-		/* write outs prompt only if from standard input */
-		if (isatty(STDIN_FILENO))
+		length = 0; /* reset length and buffer for getline funciton */
+		buffer = NULL;
+		if (isatty(STDIN_FILENO)) /* write outs prompt only if from standard input */
 			write(STDOUT_FILENO, prompt, 2);
 	}
 	if (characters == -1)
